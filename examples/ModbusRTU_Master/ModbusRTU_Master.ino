6a39d648f6083cde6df1562412bd1901087eed16
Canopus <49629370+NamNamIoT@users.noreply.github.com>
2025-07-06 01:43:43 +0700

    Update ModbusRTU_Master.ino

--- examples/ModbusRTU_Master/ModbusRTU_Master.ino
+++ examples/ModbusRTU_Master/ModbusRTU_Master.ino
@@ -1,34 +1,35 @@
-#include <Rak3172_Canopus.h>
 #include "Canopus_Modbus.h"
 ModbusMaster node;
-
 uint8_t result;
 void setup()
 {
+  //Enable power for external sensor
+  pinMode(PB5, OUTPUT);
+  digitalWrite(PB5, HIGH);
+
+  #Led PA8 as output
+  pinMode(PA8, OUTPUT);
   Serial.begin(115200);
-  Serial.print("\r\n*****************RAK3172_CANOPUS*******************");
+  Serial.print("\r\n*****************ArduLora*******************");
   Serial_Canopus.begin(9600, SERIAL_8N1);
-  init_io();
-  enable_Vrs485();
 }
 void loop()
 {
   //***************READ node 1**************************
-  node.begin(1, Serial_Canopus); // slave ID node
+  node.begin(1, Serial_Canopus); //slave ID node
   Serial.printf("");
-  Serial.printf("\r\n\n\nExample read modbus RTU for RAK3172_Canopus board");
+  Serial.printf("\r\n\n\nExample read modbus RTU for ArduLora board");
 
-  result = node.readHoldingRegisters(0, 10); // Read 40000 to 40009
+  result = node.readHoldingRegisters(0, 10);//Read 40000 to 40009
   delay(10);
-  if (result == node.ku8MBSuccess) // Read success
+  if (result == node.ku8MBSuccess) //Read success
   {
-    for (uint8_t i = 0; i < 10; i++)
+    for (uint8_t i = 0; i < 10; i ++ )
     {
       Serial.printf("\r\nValue 4000%d: %d", i, node.getResponseBuffer(i));
     }
   }
-  else
-    Serial.printf("\nRead Fail node 1");                 // read fail
-  digitalWrite(LED_SYNC, !digitalRead(LED_SYNC)); // blink led
+  else Serial.print("Read Fail node 1"); //read fail
+  digitalWrite(PA8, !digitalRead(PA8)); //blink led
   delay(500);
 }
