3426b88e270181703ff78b3e0205cb5c99b12436
Canopus <49629370+NamNamIoT@users.noreply.github.com>
2025-07-06 01:44:39 +0700

    Update SHT3X.ino

--- examples/SHT3X/SHT3X.ino
+++ examples/SHT3X/SHT3X.ino
@@ -1,42 +1,32 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <ArtronShop_SHT3x.h>
-#include <Rak3172_Canopus.h>
+#include <Arduino.h>  // Include the Arduino core library.
+#include <Wire.h>  // Include the Wire library for I2C communication.
+#include <ArtronShop_SHT3x.h>  // Include the SHT3x library.
+ArtronShop_SHT3x sht3x(0x44, &Wire);  // ADDR: 0 => 0x44, ADDR: 1 => 0x45
 
-ArtronShop_SHT3x sht3x(0x44, &Wire); // ADDR: 0 => 0x44, ADDR: 1 => 0x45
-
-void setup()
-{
-  Serial.begin(115200);
-  Serial.print("\r\n************RAK3172_CANOPUS**************");
-  init_io();
-  enable_Vss3();
-  disable_Vss5();
-  disable_Vss12();
-
-  Wire.begin();
-  if (!sht3x.begin())
-  {
-    Serial.println("SHT3x not found !");
-    delay(1000);
-  }
+void setup() {
+Serial.begin(115200);  // Initialize serial communication at 115200 baud rate.
+Serial.print("\r\n************ArduLora**************");  // Print a message indicating the start of the program.
+//Enable power for external sensor
+pinMode(PB5, OUTPUT);
+digitalWrite(PB5, HIGH);
+delay(100);  // Wait for 100 milliseconds.
+Wire.begin();  // Initialize the I2C communication.
+while (!sht3x.begin()) {  // Check if SHT3x sensor is detected.
+  Serial.println("SHT3x not found !");  // Print a message if SHT3x sensor is not detected.
+  delay(1000);  // Wait for 1 second before retrying.
+}
 }
 
-void loop()
-{
-  if (sht3x.measure())
-  {
-    Serial.print("Temperature: ");
-    Serial.print(sht3x.temperature(), 1);
-    Serial.print(" *C\tHumidity: ");
-    Serial.print(sht3x.humidity(), 1);
-    Serial.print(" %RH");
-    Serial.println();
-  }
-  else
-  {
-    sht3x.begin();
-    Serial.println("SHT3x read error");
-  }
-  delay(1000);
+void loop() {
+if (sht3x.measure()) {  // Check if the measurement is successful.
+  Serial.print("Temperature: ");  // Print a label indicating the temperature measurement.
+  Serial.print(sht3x.temperature(), 1);  // Print the temperature value with one decimal place.
+  Serial.print(" *C\tHumidity: ");  // Print a label indicating the humidity measurement.
+  Serial.print(sht3x.humidity(), 1);  // Print the humidity value with one decimal place.
+  Serial.print(" %RH");  // Print unit (% relative humidity).
+  Serial.println();  // Print a newline character.
+} else {
+  Serial.println("SHT3x read error");  // Print a message if there is an error reading from the SHT3x sensor.
+}
+delay(1000);  // Wait for 1 second before the next measurement.
 }
